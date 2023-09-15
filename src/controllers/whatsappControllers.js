025728ea97a646bfa522155214732ad00b32b3ac
Xreflextion <lillian.ye06@gmail.com>
2023-09-15 16:25:55 -0700

    verify whatsapp

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,17 @@
-const verifyToken = (req, res) => { 
-    res.send('verifyToken');
+const verifyToken = (req, res) => {  // req is what input we receive, res is what output we send
+    try {
+        var accessToken = 'bwaap'
+        var token =  req.query['hub.verify_token'];
+        var challenge = req.query['hub.challenge'] // the output we should be sending (accessToken = token)
+
+        if (challenge != null && token != null && token == accessToken) {
+            res.send(challenge)
+        } else {
+            res.status(400).send()
+        }
+    } catch (e) {
+        res.status(400).send()
+    }
 }
 
 const receiveMessage = (req, res) => {
--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use('/whatsapp', apiRouter);
 
 // running app on provided port
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
 });
